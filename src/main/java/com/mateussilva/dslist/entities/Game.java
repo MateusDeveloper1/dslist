90f458f66a6a3ebfd18cd42d457b295c0f015c5a
Mateus Silva <mateusilva767@gmail.com>
2023-05-09 13:14:52 -0300

    Game seed

--- src/main/java/com/mateussilva/dslist/entities/Game.java
+++ src/main/java/com/mateussilva/dslist/entities/Game.java
@@ -21,21 +21,25 @@ public class Game {
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
-	private String platforms;
+	private String platform;
 	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {}
 
-	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String platform, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.platforms = platforms;
+		this.platform = platform;
 		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
@@ -75,11 +79,11 @@ public class Game {
 	}
 
 	public String getPlatforms() {
-		return platforms;
+		return platform;
 	}
 
-	public void setPlatforms(String platforms) {
-		this.platforms = platforms;
+	public void setPlatforms(String platform) {
+		this.platform = platform;
 	}
 
 	public Double getScore() {
